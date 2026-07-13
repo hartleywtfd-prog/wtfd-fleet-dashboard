bd2b38e60bc727068d96dd6527adaa75ced793d3
hartleywtfd-prog <hartleywtfd@gmail.com>
2026-07-13 09:43:40 -0400

    Update dashboard.js

--- functions/api/dashboard.js
+++ functions/api/dashboard.js
@@ -1,6 +1,6 @@
 export async function onRequestGet() {
   const url =
-    "https://script.google.com/macros/s/AKfycbwhinuB6R-rxHMG4lSkilihzVcFUrGXOQNbhYLrNQfksn-Yy5nxOPFyaUNnRhlpIhRGhw/exec?mode=json&action=sync";
+    "https://script.google.com/macros/s/AKfycbwhinuB6R-rxHMG4lSkilihzVcFUrGXOQNbhYLrNQfksn-Yy5nxOPFyaUNnRhlpIhRGhw/exec?mode=json";
 
   const response = await fetch(url, { redirect: "follow" });
   const text = await response.text();
